f37d435b6b5ff192ac947f2c20cd6427c9948ef3
Anthony <anthony.gilbert93@gmail.com>
2022-01-03 21:32:48 -0600

    Added table and most of the data for users to the table

--- src/App.vue
+++ src/App.vue
@@ -1,124 +1,132 @@
 <template>
- <div class="row">
-        <div class="col-md-12">
-
-            
-            <form onSubmit="return false;" id="userForm">
-        
-              <h1 class="h3 mb-3 font-weight-normal">Search 4 A GitHub User</h1>
-              <input type="text" id="inputUser" class="form-control"  name="username" value="" placeholder="User Name" required autofocus>
-              <br>
-              <button class="btn btn-lg btn-primary btn-block" type="button" id="searchUser" onclick="clickButton()">Search</button>
-              
-            </form><br>
-            <div>
-                <h3 class="header">Found Users</h3>
-                <p id="foundUsers"></p>
-            </div>
-        
-        
-                <table id="results_table" class="table" width="100%">
-                    <thead>
-                      <tr>
-                        <th class="th-sm">Avatar
-                        </th>
-                        <th class="th-sm">Name
-                        </th>
-                        <th class="th-sm">Bio
-                        </th>
-                        <th class="th-sm">Followers
-                        </th>
-                        <th class="th-sm">Following
-                        </th>
-                        <th class="th-sm">Twitter
-                        </th>
-                        <th class="th-sm">Github Link
-                        </th>
-                      </tr>
-                    </thead>
-                     <tbody>
-                      
-                    </tbody>
-        
-                    
-                  </table>
-                <p class="mt-5 mb-3 text-muted">Made with <img alt="Vue logo" src="./assets/logo.png"/>ue.js  by Anthony Gilbert</p>
-                
+  <div class="row">
+    <div class="col-md-12">
+      <form onSubmit="return false;" id="userForm">
+        <h1 class="h3 mb-3 font-weight-normal">Search 4 A GitHub User</h1>
+        <input
+          type="text"
+          id="inputUser"
+          class="form-control"
+          name="username"
+          v-model="username"
+          placeholder="User Name"
+          required
+          autofocus
+        />
+        <br />
+        <p>{{ username }}</p>
+        <button
+          class="btn btn-lg btn-primary btn-block"
+          type="button"
+          id="searchUser"
+          @click.prevent="handleSubmit"
+        >
+          Search
+        </button>
+      </form>
+      <br />
+      <div class="container mt-4">
+        <h3 class="header">Found Users</h3>
+        <div v-if="users">
+          <p id="foundUsers"></p>
+          <table id="results_table" class="table" width="100%">
+            <thead>
+              <tr>
+                <th class="th-sm">Avatar</th>
+                <th class="th-sm">Name</th>
+                <th class="th-sm">Bio</th>
+                <th class="th-sm">Followers</th>
+                <th class="th-sm">Following</th>
+                <th class="th-sm">Twitter</th>
+                <th class="th-sm">Github Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              <!-- <tr> -->
+              <tr v-for="user in users" :key="user.index">
+                <td><img v-bind:src="user.avatar_url" alt="" /></td>
+                <td>{{ user.name }}</td>
+                <td>{{ user.bio }}</td>
+                <td>{{ user.followers }}</td>
+                <td>{{ user.following }}</td>
+                <td>{{ user.twitter_username }}</td>
+                <td>{{ user.html_url }}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-
+        <div v-else><p>No users found</p></div>
+      </div>
+      <p class="mt-5 mb-3 text-muted">
+        Made with <img alt="Vue logo" src="./assets/logo.png" />ue.js by Anthony
+        Gilbert
+      </p>
     </div>
-
+  </div>
 </template>
 
 <script>
-
+import "jquery/dist/jquery.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "datatables.net-dt/js/dataTables.dataTables";
+import "datatables.net-dt/css/jquery.dataTables.min.css";
+import axios from "axios";
+import $ from "jquery";
+// import { ref } from "vue";
+import { ref } from "@vue/reactivity";
 
 export default {
-  methods: {
-    clickButton() {
+  setup() {
+    const username = ref("");
+    const users = ref(null);
 
-    //    const api = 'https://api.github.com/search/users?q=';
-    // const inputValue = document.querySelector('#inputUser').value;
-    // const apiUser = api + inputValue;
+    const handleSubmit = async () => {
+      const gh_api = "https://api.github.com/search/users?q=";
+      const userInput = username.value;
+      const gh_endpoint = gh_api + userInput;
 
-    // table.clear().draw();
+      await axios.get(gh_endpoint).then((res) => {
+        const axiosRequest = res.data;
 
-    // fetch(apiUser)
-    //     .then(response => {
+        if (typeof axiosRequest === "object") {
+          const newUserArray = [];
+          newUserArray.push(axiosRequest);
+          console.log(newUserArray);
 
-    //         return response.json();
-    //     }).then(response => {
+          for (let individualUser in newUserArray[0].items) {
+            const anotherNewUserArray = [];
+            if (typeof newUserArray[0].items[individualUser] === "object") {
+              anotherNewUserArray.push(newUserArray[0].items[individualUser]);
 
-    //         for (var userIdx in response.items) {
-    //             const userEP = 'https://api.github.com/users/';
-    //             fetch(userEP + response.items[userIdx].login)
-    //                 .then(function (response) {
+              console.log(anotherNewUserArray);
+            } else {
+              return;
+            }
+            console.log();
+            // return individualUser;
+            users.value = newUserArray[0].items;
+          }
+        } else {
+          console.log("hello from else statement");
+          // for (let individualUser in axiosRequest[0].items) {
+          for (let individualUser in users[0].index) {
+            console.log(individualUser);
+          }
+          users.value = res.data;
+        }
+        console.log("axiosRequest: ", axiosRequest);
 
-    //                     return response.json();
-    //                 }).then(response => {
+        $("#results_table").DataTable();
+        return users;
+      });
+    };
 
-
-    //                     table.row.add([
-    //                         '<img class="useravatar" src="' + response.avatar_url + '"></img>',
-    //                         response.name,
-    //                         response.bio,
-    //                         response.followers,
-    //                         response.following,
-    //                         response.twitter_username,
-    //                         '<a target=_blank" href="' + response.html_url + '">' + response.html_url + '</a>'
-    //                     ]).draw();
-
-    //                 })
-    //         }
-    //     });
-
-    },
-    mounted(){
-
-//       this.dataTable = $(document).ready(function (e) {
-//     const api = 'https://api.github.com/users/';
-//     const input = document.querySelector('#inputUser');
-//     const inputValue = input.value;
-//     const apiUser = api + inputValue;
-
-//     table = $('#results_table').DataTable();
-//     input.addEventListener("keyup", function (event) {
-
-//         if (event.keyCode == 13) {
-//             event.preventDefault();
-
-//             console.log(e);
-//             clickButton();
-//         }
-//     });
-// });
-
-    }
+    return { handleSubmit, username, users };
   },
-}
+};
 </script>
 
-<style>
+<style scoped>
 img {
   width: 20px;
 }
@@ -127,26 +135,29 @@ img {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-
 }
-body {
-    background-color: #061328;
-    color: #ffff;
-}
+body,
+html,
 td {
-    color: #ffff;
+  background-color: #061328 !important;
+  color: #ffff;
 }
+
 tr {
-    color: turquoise;
+  color: turquoise;
 }
-
-
 .useravatar {
-    width: 100px;
-    height: 100px;
+  width: 100px;
+  height: 100px;
 }
-
 h3 {
-    text-decoration: underline;
+  text-decoration: underline;
+}
+.container {
+  max-width: 1000px;
+}
+#results_table {
+  height: auto;
 }
 </style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Form />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Form from "@/components/Form.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Form,
+  },
+};
 </script>
 
-<style scoped>
-
+<style>
 </style>
 
