1667ac9b14314db3ad18dcb599c2b953b5f75c8b
Anthony Gilbert <anthony.gilbert93@gmail.com>
2025-03-21 05:46:30 -0500

    Converted the Vite configuration to CommonJS format

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+const { defineConfig } = require('vite')
+const vue = require('@vitejs/plugin-vue')
+const path = require('path')
 
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
