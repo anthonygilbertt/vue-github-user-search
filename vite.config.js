565f46a156150691f438acaa9ff2351048f19278
Anthony Gilbert <anthony.gilbert93@gmail.com>
2025-03-21 06:00:00 -0500

    Created a custom CommonJS build script

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-const { defineConfig } = require('vite')
-const vue = require('@vitejs/plugin-vue')
-const path = require('path')
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-module.exports = defineConfig({
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': resolve(__dirname, './src')
     }
   }
 })
