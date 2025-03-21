07f1495003e24bb651c8e787139aa6f16959ce8f
Anthony Gilbert <anthony.gilbert93@gmail.com>
2025-03-21 06:42:00 -0500

    Created a toml file for netlify

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
+const { defineConfig } = require('vite')
+const vue = require('@vitejs/plugin-vue')
+const path = require('path')
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, 'src')
     }
   }
 })
